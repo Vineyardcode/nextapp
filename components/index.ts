3fa9b7025bdee6bf037a893ee72c7a7ba469d6a3
Vineyardcode <108179228+Vineyardcode@users.noreply.github.com>
2023-06-11 16:46:27 +0200

    master

---
+++ components/index.ts
@@ -0,0 +1,21 @@
+import CustomButton from "./CustomButton";
+import Hero from "./Hero";
+import Footer from "./Footer";
+import NavBar from "./Navbar";
+// import ShowMore from "./ShowMore";
+// import SearchBar from "./Searchbar";
+// import CustomFilter from "./CustomFilter";
+// import CarCard from "./CarCard";
+
+
+
+export {
+  Hero,
+  CustomButton,
+  Footer,
+  NavBar,
+  // ShowMore,
+  // SearchBar,
+  // CarCard,
+  // CustomFilter,
+};
